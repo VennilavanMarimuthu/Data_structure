6723d8d7732aa3f2de873dd6f551cee035ef368a
VennilavanMarimuthu <marimuthuvennilavan@gmail.com>
2024-05-28 12:36:25 +0530

    fix : fixed error

--- Main.java
+++ Main.java
@@ -1,20 +1,7 @@
 class Main{
     public static void main(String[] args)throws Exception{
 
-          DynamicQueue  d=new DynamicQueue(4);
-
-          d.enqueue(1);
-          d.enqueue(2);
-          d.enqueue(3);
-          d.enqueue(4);
-          d.enqueue(5);
-          d.enqueue(6);
-
-          d.display();
-          System.out.println(d.dequeue());
-
-          System.out.println(d.peek());
-
+    
         
 
     }
